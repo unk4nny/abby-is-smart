f414754f024e62fadb7272daedb741840b40552c
unk4nny <abigail.r.shear@gmail.com>
2022-09-03 13:39:00 -0400

    Added user avatar command

--- deploy-commands.js
+++ deploy-commands.js
@@ -1,20 +1,43 @@
-import { config } from 'dotenv';
+import { config } from "dotenv";
 
-import { SlashCommandBuilder, Routes } from 'discord.js';
-import { REST } from '@discordjs/rest';
+import {
+  SlashCommandBuilder,
+  Routes,
+  SlashCommandUserOption,
+} from "discord.js";
+import { REST } from "@discordjs/rest";
 
-config()
+config();
 
 const commands = [
-	new SlashCommandBuilder().setName('ping').setDescription('HELP im bad'),
-	new SlashCommandBuilder().setName('server').setDescription('Replies with server info!'),
-	new SlashCommandBuilder().setName('user').setDescription('Replies with user info!'),
-	new SlashCommandBuilder().setName('capybara').setDescription('yes')
-]
-	.map(command => command.toJSON());
+  new SlashCommandBuilder()
+    .setName("avatar")
+    .setDescription("Sends a user avatar")
+    .addUserOption(
+      (option) =>
+        option
+          .setName("user")
+          .setDescription(
+            "AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA"
+          )
+      // .setRequired()
+    ),
+  new SlashCommandBuilder()
+    .setName("capybara")
+    .setDescription("Sends a Capybara image!"),
+].map((command) => command.toJSON());
 
-const rest = new REST({ version: '10' }).setToken(process.env.BOT_TOKEN);
+const rest = new REST({ version: "10" }).setToken(process.env.BOT_TOKEN);
 
-rest.put(Routes.applicationGuildCommands(process.env.CLIENT_ID, process.env.GUILD_ID), { body: commands })
-	.then((data) => console.log(`Successfully registered ${data.length} application commands.`))
-	.catch(console.error);
+rest
+  .put(
+    Routes.applicationGuildCommands(
+      process.env.CLIENT_ID,
+      process.env.GUILD_ID
+    ),
+    { body: commands }
+  )
+  .then((data) =>
+    console.log(`Successfully registered ${data.length} application commands.`)
+  )
+  .catch(console.error);
--- index.js
+++ index.js
@@ -1,40 +1,47 @@
-import { config } from 'dotenv';
-import { Client, GatewayIntentBits, MessagePayload } from 'discord.js';
-import axios from "axios"
+import { config } from "dotenv";
+import { Client, GatewayIntentBits, MessagePayload } from "discord.js";
+import axios from "axios";
 
-config()
+config();
 
 // Create a new client instance
 const client = new Client({ intents: [GatewayIntentBits.Guilds] });
 
 // When the client is ready, run this code (only once)
-client.once('ready', () => {
-	console.log('Ready!');
+client.once("ready", () => {
+  console.log("Ready!");
 });
 
-client.on('interactionCreate', async interaction => {
-	if (!interaction.isChatInputCommand()) return;
+client.on("interactionCreate", async (interaction) => {
+  if (!interaction.isChatInputCommand()) return;
 
-	const { commandName } = interaction;
+  const { commandName } = interaction;
 
-	if (commandName === 'ping') {
-		await interaction.reply('pong!');
-	} else if (commandName === 'server') {
-		await interaction.reply('Server info.');
-	} else if (commandName === 'user') {
-		await interaction.reply('User info.');
-	} else if (commandName === 'capybara') {
-		await capy(interaction)
-	}
+  if (commandName === "avatar") {
+    await avi(interaction);
+  } else if (commandName === "capybara") {
+    await capy(interaction);
+  }
 });
 
 // Login to Discord with your client's token
 client.login(process.env.BOT_TOKEN);
 // If it uses await use async yes big brain
 async function capy(interaction) {
-	const response = await axios.get("https://api.capy.lol/v1/capybara/", {
-        responseType: 'arraybuffer'
-    })
-	const image = response.data
-	await interaction.reply(new MessagePayload(interaction,{files: [image]}))
+  const response = await axios.get("https://api.capy.lol/v1/capybara/", {
+    responseType: "arraybuffer",
+  });
+  const image = response.data;
+  await interaction.reply(new MessagePayload(interaction, { files: [image] }));
+}
+
+async function avi(interaction) {
+  const user = interaction.options.getUser("user");
+  if (user) {
+    const avatar = user.avatarURL();
+    await interaction.reply(avatar);
+  } else {
+    const avatar = interaction.member.avatarURL();
+    await interaction.reply(avatar);
+  }
 }
